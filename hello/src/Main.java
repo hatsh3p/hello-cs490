0c94feb9f1ffa710067e9ae432227385ee517078
hatsh3p <sarahamarragy@gmail.com>
2023-01-31 14:04:54 -0800

    Added more exclamation points.

--- hello/src/Main.java
+++ hello/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Howdy from CS490!");
+        System.out.println("Howdy from CS490!!!!!!!!!!");
     }
 }
